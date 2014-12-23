05f33dd4fed2b1396edc331366401cfddcd9fd7e
Steren <steren.giannini@gmail.com>
2014-12-23 10:24:16 +0100

    New version using Polymer and Material Design look. Defined web components for Work item, Work service, and Work list.

--- js/services.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* Services */
-angular.module('portfolio.services', ['ngResource']).
-	factory('Work', function($resource){
-		return $resource('data.json', {}, {
-			query: {method:'GET', isArray:true}
-		});
-	});
